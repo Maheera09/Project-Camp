fc149a5d7f8b9f9a363a3f94cab890b34fb944c9
Maheera <maheerasaqib089@gmail.com>
2026-08-11 17:25:01 +0500

    start section:22

--- src/db/index.js
+++ src/db/index.js
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 
 // mongoose.connect(process.env.MONGO_URI);
 //we can use this method but a lot
+import dns from "dns";
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const connectDB = async () => {
   try {
